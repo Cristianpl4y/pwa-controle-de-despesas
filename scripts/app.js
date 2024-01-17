4df685780d209caaf1b05a40c5ace29016d38edf
Cristian <cristianpl4y@gmail.com>
2024-01-16 21:35:48 -0300

    :bug: fix: Erro ao registrar Service Worker: TypeError: Failed to register a ServiceWorker

--- scripts/app.js
+++ scripts/app.js
@@ -197,7 +197,7 @@ transactionUl.addEventListener('click', (event) => {
 
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
     .then(registration => {
       console.log('Service Worker registrado com sucesso:', registration);
     })
